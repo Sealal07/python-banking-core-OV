a076780e30988c7fe3cd7ab0e62dc78f501cab63
Student <Student@ItTopNSK.local>
2026-08-04 16:24:48 +0300

    feat: setup engine and config

---
+++ database.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy.ext.asyncio import(
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine
+)
+from sqlalchemy.orm import DeclarativeBase
+
+DB_URL = 'sqlite+aiosqlite:///./database.db'
+
+engine = create_async_engine(DB_URL, echo=False)
+async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
